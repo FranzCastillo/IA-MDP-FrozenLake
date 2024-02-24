a48e0ba4e22944780c8d7e1d20a2c60cc0362a7e
Francisco Castillo <castillocerna@gmail.com>
2024-02-23 23:51:28 -0600

    Not Needed State Attributes Removed

--- src/state.py
+++ src/state.py
@@ -1,13 +1,6 @@
 class State:
     def __init__(self, x, y):
         self.value = (x, y)
-        self.reward = {
-            'up': 0,
-            'down': 0,
-            'left': 0,
-            'right': 0
-        }
-        self.probability = {}
 
     def __repr__(self):
         return f'State{self.value}'
